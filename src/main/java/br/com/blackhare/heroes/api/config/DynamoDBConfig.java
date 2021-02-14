1bd8a849f78f4eee17d4f73d1c2bff2aa15d7baf
Giovanni Bayerlein <giovannibayerlein@hotmail.com>
2021-02-14 19:36:29 -0300

    desfazendo a cagada

--- src/main/java/br/com/blackhare/heroes/api/config/DynamoDBConfig.java
+++ src/main/java/br/com/blackhare/heroes/api/config/DynamoDBConfig.java
@@ -6,11 +6,16 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class DynamoDBConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     public DynamoDBMapper mapper() {
@@ -22,7 +27,7 @@ public class DynamoDBConfig {
                 .standard()
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration("dynamodb.sa-east-1.amazonaws.com", "sa-east-1"))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAYRXJMXQYKP3I52F4", "AupF7O1WrAO9Nkli9Bhc7Jrz6kJ+cDMrnlDxDxqF")))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(environment.getProperty("access"), environment.getProperty("secret"))))
                 .build();
     }
 }
